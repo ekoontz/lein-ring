1dcb28d8f90cb7eaed639265daa8bd2b6cf0bf81
Eugene Koontz <ekoontz@hiro-tan.org>
2017-05-28 17:03:57 -0700

    use my own s3 repo

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
-(defproject lein-ring "0.9.8-SNAPSHOT"
+(defproject lein-ring "0.9.8-ekoontz"
   :description "Leiningen Ring plugin"
+  :plugins [[s3-wagon-private "1.2.0"]]
+
   :url "https://github.com/weavejester/lein-ring"
   :scm {:name "git"
         :url "https://github.com/weavejester/lein-ring"}
@@ -7,4 +9,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/data.xml "0.0.8"]
                  [leinjacker "0.4.2"]]
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+
+  :repositories {"s3" {:url "s3p://ekoontz-repo/releases/"
+                       :username :env/aws_access_key ;; gets environment variable AWS_ACCESS_KEY
+                       :passphrase :env/aws_secret_key ;; gets environment variable AWS_SECRET_KEY
+                       }})
+
+  
